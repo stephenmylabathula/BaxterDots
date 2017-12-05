47051d94ac3b17dce2ae74ea205c006a6fc0b4d7
Stephen Mylabathula <mylo777@icloud.com>
2017-12-05 11:24:05 -0500

    adds marker drop

--- src/BaxterConnectDots/connect_dots_v1.py
+++ src/BaxterConnectDots/connect_dots_v1.py
@@ -295,7 +295,7 @@ def pixel_to_baxter(px, dist, cam_calib=0.0025, cam_x_offset=0.045, cam_y_offset
 
 def main():
 
-    overhead_pose = [0.6, 0.0, 0.1, -math.pi, 0.0, 0.0]
+    overhead_pose = [0.6, 0.0, 0.1, math.pi, 0.0, 0.0]      # Change Yaw for Wrist Rotation
 
     # Move Out Right Arm
     print "Moving Right Arm to Side"
@@ -347,6 +347,7 @@ def main():
 
     # Move Out Right Arm
     print "Moving Right Arm to Side"
+    motion.open_gripper()
     pose = [0.2, -1.0, 0.2, -math.pi, 0.0, 0.0]  # Right Arm to Side Pose
     motion.ik_move('right', pose)
     time.sleep(1)
